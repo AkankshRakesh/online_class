c5c94d2a6bb63be7233907c67040de41dec73452
AkankshRakesh <akankshrakesh@gmail.com>
2024-08-31 13:43:14 +0530

    Your commit

--- day01week1.cpp
+++ day01week1.cpp
@@ -5,38 +5,8 @@ using namespace std;
 int main() {
     string str1, str2, concatenatedString;
 
-    cout << "Enter the first string: ";
-    cin >> str1;
-    cout << "Enter the second string: ";
-    cin >> str2;
-
-    concatenatedString = str1 + str2;
-    cout << "Concatenated string: " << concatenatedString << endl;
-
-    int grade1, grade2, grade3, grade4, grade5;
-    float average;
-
-    cout << "\nEnter grade 1: ";
-    cin >> grade1;
-    cout << "Enter grade 2: ";
-    cin >> grade2;
-    cout << "Enter grade 3: ";
-    cin >> grade3;
-    cout << "Enter grade 4: ";
-    cin >> grade4;
-    cout << "Enter grade 5: ";
-    cin >> grade5;
-
-    average = (grade1 + grade2 + grade3 + grade4 + grade5) / 5.0;
-    cout << "The average grade is: " << average << endl;
-
-    int number, square;
-
-    cout << "\nEnter a number: ";
-    cin >> number;
-
-    square = number * number;
-    cout << "The square of " << number << " is: " << square << endl;
+    vector<int> temp = {1, 2,3 ,4};
+    temp.
 
     return 0;
 }
